f92589aaa4d0e87bff951be5f71ec79b6e3fb471
Zmeev06 <88145578+Zmeev06@users.noreply.github.com>
2023-01-28 09:33:28 +0300

    complete

--- js/fetch.js
+++ js/fetch.js
@@ -1,32 +1,37 @@
-function sendRequest(url, method, body = null) {
-  return fetch(url, {
-    method: method,
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((response) => {
-    return response.json();
-  });
+async function sendRequest(url, method, body = null) {
+  try {
+    const data = await fetch(url, { method: method, mode: "cors" });
+    return data.json();
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
-
+const success = document.querySelector(".success");
+const successText = document.querySelector(".success__text");
 const btn = document.querySelector(".login-form__btn");
 const login = document.querySelector(".login-form__login");
 const pass = document.querySelector(".login-form__pass");
 
 btn.addEventListener("click", () => {
-  if (login.value.trim() !== "" || pass.value.trim() !== "") {
-    loginValue = login.value;
-    passValue = pass.value;
-    login.value = ''
-    pass.value = ''
-    const requestUrl = `https://test-works.pr-uni.ru/api/login/index.php?login=${loginValue}&password=${passValue}`;
-  sendRequest(requestUrl, "GET")
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-  } else {
-    alert("Введите логин и пароль!")
-  }
-  
+  login.disabled = true;
+  pass.disabled = true;
+  document.querySelector(".preloader").style.display = "block";
+  const requestUrl = `https://test-works.pr-uni.ru/api/login/index.php?login=${login.value}&password=${pass.value}`;
+
+  sendRequest(requestUrl, "get").then((response) => {
+    if (response.status === "ok") {
+      document.querySelector(".preloader").style.display = "none";
+      successText.textContent = `${response.user.name}, Вы успешно авторизованы!`;
+      success.style.display = "flex";
+      document.cookie = `token=${response.token}`;
+    } else if (response.status ==="error") {
+      document.querySelector(".preloader").style.display = "none";
+      document.querySelector('.login-form__error').textContent=response.errorMessage;
+      document.querySelector('.login-form__error').style.display = "block";
+      login.disabled = false;
+      pass.disabled = false;
+      throw new Error(response.errorCode);
+    }
+  });
 });
